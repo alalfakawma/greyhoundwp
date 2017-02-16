5e3250d975a9bfe28a52dc2ae69bf176c831ba7b
alalfakawma <alalfakawma@gmail.com>
2017-02-17 00:05:37 +0530

    renewed

---
+++ greyhound/footer.php
@@ -0,0 +1,30 @@
+	</div>
+	<!-- END CONTENT -->
+	<?php get_sidebar(); ?>
+	<!-- END SIDEBAR -->
+</div>
+<!-- CLEARFIX -->
+<div class="clearfix"></div>
+<!-- CLEARFIX -->
+	<!-- START FOOTER -->
+	<footer id="main-footer">
+		<div class="container">
+			<div id="footer-links">
+				<span>Links</span>
+				<ul>
+					<li><a href="https://www.twitter.com/r4xee" target="_blank"><i class="fa fa-twitter"></i></a></li>
+					<li><a href="https://www.instagram.com/aseem_26/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+					<li><a href="https://github.com/alalfakawma" target="_blank"><i class="fa fa-github"></i></a></li>
+					<li><a href="https://steamcommunity.com/id/alalfakawma" target="_blank"><i class="fa fa-steam"></i></a></li>
+				</ul>
+			</div>
+
+			<div id="footer-copyright">Copyright &copy; <?php the_date("Y"); ?> Aseem Lalfakawma</div>
+		</div>
+	</footer>
+	<!-- END FOOTER -->
+	<?php wp_footer(); ?>
+
+<script src="https://use.fontawesome.com/cbd153a72f.js"></script>
+</body>
+</html>
